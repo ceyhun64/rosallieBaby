29d4e7537440ea93c03127c2efb098dc8982b038
Ceyhun Turkmen <ctrkmn64@gmail.com>
2025-09-17 11:28:47 +0300

    6. commit

--- components/layout/topbar.jsx
+++ components/layout/topbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Topbar() {
   return (
-    <div className="w-full bg-[#ffb14b] py-4 text-center sticky top-0 z-50">
+    <div className="w-full bg-[#ffb14b] py-2 text-center sticky top-0 z-50">
       <p className="text-md font-medium text-white">
         100% organic cotton muslin sets are waiting for you
       </p>
