2fe8f6021736d0fafc80cf12fcf22f164289ebc3
Ceyhun Turkmen <ctrkmn64@gmail.com>
2025-09-17 11:32:29 +0300

    7. commit

--- components/layout/topbar.jsx
+++ components/layout/topbar.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { useIsMobile } from "@/hooks/use-mobile";
 
 export default function Topbar() {
+  const isMobile = useIsMobile();
   return (
-    <div className="w-full bg-[#ffb14b] py-2 text-center sticky top-0 z-50">
-      <p className="text-md font-medium text-white">
+    <div className="w-full bg-[#ffb14b] py-2 text-center sticky top-0 z-50  ">
+      <p
+        className={` font-medium text-white${isMobile ? "text-xs" : "text-md"}`}
+      >
         100% organic cotton muslin sets are waiting for you
       </p>
     </div>
