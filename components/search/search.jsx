67fb00c31be988748dff61f877bc1cb1c486c5f2
Ceyhun Turkmen <ctrkmn64@gmail.com>
2025-12-07 15:46:42 +0300

    52. commit

--- components/search/search.jsx
+++ components/search/search.jsx
@@ -54,7 +54,7 @@ export default function DefaultSearch() {
                 className="text-[16px] md:text-[26px] tracking-[0.04em] text-gray-900 font-serif font-extralight hover:opacity-80 transition-all"
               >
                 <Image
-                  src="/logo/logo2.png"
+                  src="/logo/logo2.webp"
                   alt="Logo"
                   width={84}
                   height={80}
